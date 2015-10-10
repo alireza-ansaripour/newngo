1d22f9790c500d18228211dfac31fd5a961d9696
alireza <alireza>
2015-10-10 19:26:02 +0330

    ngo website

--- Ngo/settings.py
+++ Ngo/settings.py
@@ -71,8 +71,8 @@ WSGI_APPLICATION = 'Ngo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+        'ENGINE': 'django.db.backends.mysql',
+
     }
 }
 
